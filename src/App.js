39260d3e189e620d82da349c51ef19c3f6e377fc
Margarita-06 <mapesa0609@gmail.com>
2025-07-14 11:35:26 -0500

    Componentes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import RegisterPage from './pages/RegisterPage/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 
+import NotFoundPage from './pages/Components/NotFound';
+import ProtectedRoute from './pages/Components/ProtectedRoute';
+
 function App() {
   return (
     <BrowserRouter>
-  <Routes>
-    <Route path="/" element ={<LoginPage/>}/>
-    <Route path="/Register" element ={<RegisterPage/>}/>
-    <Route path="/Forgot" element ={<ForgotPasswordPage/>}/>
-    <Route path="/Dashboard" element ={<DashboardPage/>}/>
-  
-  </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/Forgot" element={<ForgotPasswordPage />} />
+        <Route path="/dashboard" element={<ProtectedRoute> <DashboardPage /> </ProtectedRoute>} />
+
+        <Route path="*" element={<NotFoundPage />} />
+
+      </Routes>
+    </BrowserRouter>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
